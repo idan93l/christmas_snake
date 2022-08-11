b196fc242a45c5e4002826a3525035795b23bb52
Idan Lev <idan.93lev@gmail.com>
2022-08-11 23:18:18 +0300

    major change - styled components and better app structure

--- src/components/Particles/ParticlesBackground.jsx
+++ src/components/Particles/ParticlesBackground.jsx
@@ -4,16 +4,12 @@ import background from "../../assests/christmas_background.png"
 
 const ParticlesBackground = () => {
   const particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
+    // console.log(main);
     await loadFull(main);
   };
 
   const particlesLoaded = (container) => {
-    console.log(container);
+    // console.log(container);
   };
 
   return (
