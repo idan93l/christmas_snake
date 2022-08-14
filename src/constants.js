9dd9970377364c890d8f9f36c584ccb294777ba5
Idan Lev <idan.93lev@gmail.com>
2022-08-14 06:37:37 +0300

    deletion of unused file constants

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-const canvasX = 1000
-const canvasY = 1000
-const initialSnake = [ [ 4, 10 ], [ 4, 10 ] ]
-const initialPresent = [ 14, 10 ]
-const scale = 50
-const timeDelay = 100
-
-export default {canvasX, canvasY, initialSnake, initialPresent, scale, timeDelay};
